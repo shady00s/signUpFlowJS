1e3b57f934bacc4f3d49af22d7d8e7847f0fe938
shady00s <64488184+shady00s@users.noreply.github.com>
2022-12-10 04:39:33 +0200

    Update session_storage.js

--- registration/scripts/utils/session_storage.js
+++ registration/scripts/utils/session_storage.js
@@ -1,6 +1,6 @@
 // save token to session storage function
 
 export default function saveEmailAndTokenToSessionStorage(emailVal){
-
+    window.sessionStorage.removeItem('userEmail')
     window.sessionStorage.setItem('userEmail',emailVal)
 }
